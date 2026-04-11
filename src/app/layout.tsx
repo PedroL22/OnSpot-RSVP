a7e239f4fca9e01ee218adbd464917c9840e473e
Pedro Lucena <pedrocontact22@gmail.com>
2026-04-11 06:32:47 -0300

    chore: remove unused font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,21 @@
 import '~/styles/globals.css'
 
 import type { Metadata } from 'next'
-import { DM_Sans, Geist } from 'next/font/google'
+import { DM_Sans } from 'next/font/google'
 import type { ReactNode } from 'react'
 
 import { cn } from '~/lib/utils'
 
-const dmSans = DM_Sans({ subsets: ['latin'], variable: '--font-sans' })
+const dmSans = DM_Sans({ subsets: ['latin'], variable: '--font-dm-sans' })
 
 export const metadata: Metadata = {
   title: 'OnSpot RSVP',
   description: 'A simple RSVP app built with Next.js and Tailwind CSS.',
 }
 
-const geist = Geist({
-  subsets: ['latin'],
-  variable: '--font-geist-sans',
-})
-
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <html className={cn(geist.variable, 'font-sans', dmSans.variable)} lang='en'>
+    <html className={cn(dmSans.variable, 'font-sans')} lang='en'>
       <body>{children}</body>
     </html>
   )
